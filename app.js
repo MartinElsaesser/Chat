86c3c870eab3e5ea6b4d12af08f34dfb281c8c2d
Martin Elsässer <marels@aon.at>
2023-01-10 21:38:16 +0100

    additions

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-	console.log('a user connected');
 	socket.on('chat message', (msg) => {
 		io.emit('chat message', msg);
 	});
